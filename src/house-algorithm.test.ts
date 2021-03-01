45ff6e1888ec0ad598bd4d7c85f524cfc12951b9
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-03-01 13:43:50 -0500

    Added long build including every single person

--- src/house-algorithm.test.ts
+++ src/house-algorithm.test.ts
@@ -1,7 +1,7 @@
 import { getAllPossibleArrangements } from "./house-algorithm";
 import { Listing, PeopleGroup } from "./types";
 
-test("thing", () => {
+test("short build", () => {
   const peopleGroup: PeopleGroup = {
     families: [],
     people: [
@@ -63,6 +63,155 @@ test("thing", () => {
                 // },
                 // {},
                 // {},
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  const results = getAllPossibleArrangements({ listings, peopleGroup });
+  console.log(results.length);
+});
+
+test("long build", () => {
+  const peopleGroup: PeopleGroup = {
+    families: [],
+    people: [
+      {
+        name: "Noodle",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Larry",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Deon",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Hanaan",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Isaac",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Kwinton",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Mackenzie",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Sam DeVine",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Sicquan",
+        gender: "male",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Bethany",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Caroline",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Mara",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Molly",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Pearl",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Sam Donermeyer",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Sam Knight",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Serena",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+      {
+        name: "Kaila",
+        gender: "female",
+        howManyOthersCanShareBed: { twin: 0, queen: 1, king: 1 },
+      },
+    ],
+  };
+  const listings: Listing[] = [
+    {
+      price: 0,
+      buildings: [
+        {
+          floors: [
+            {
+              rooms: [
+                {
+                  beds: [{ name: "king" }, { name: "twin" }, { name: "twin" }],
+                },
+                { beds: [{ name: "queen" }, { name: "queen" }] },
+              ],
+            },
+            {
+              rooms: [
+                {
+                  beds: [{ name: "king" }, { name: "twin" }, { name: "twin" }],
+                },
+              ],
+            },
+            {
+              rooms: [
+                {
+                  beds: [{ name: "king" }, { name: "twin" }],
+                },
+                {
+                  beds: [{ name: "king" }, { name: "twin" }],
+                },
+                {
+                  beds: [{ name: "king" }],
+                },
+                {
+                  beds: [
+                    { name: "twin" },
+                    { name: "twin" },
+                    { name: "twin" },
+                    { name: "twin" },
+                    { name: "twin" },
+                    { name: "twin" },
+                  ],
+                },
               ],
             },
           ],
