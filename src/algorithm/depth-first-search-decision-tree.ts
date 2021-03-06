572b712caaca90d0cae05ca34a9939ee1dcf7eae
alexjamesmalcolm <alexjamesmalcolm@gmail.com>
2021-03-06 14:09:12 -0500

    Commented out WIP code

--- src/algorithm/depth-first-search-decision-tree.ts
+++ src/algorithm/depth-first-search-decision-tree.ts
@@ -3,40 +3,42 @@ import partition from "lodash.partition";
 import uniqueIdGenerator from "./unique-id-generator";
 import { Decision } from "./common-decision-tree";
 
-export const depthFirstSearchDecisionTree = <
-  PermutableMember extends unknown,
-  CommonEnvironment extends unknown
->({
-  areBranchesEquivalent,
-  permutable,
-  shouldKeepBranch,
-  getCommonEnvironment,
-  branchingLimit = Number.MAX_SAFE_INTEGER,
-  scoreBranch,
-}: {
-  permutable: PermutableMember[];
-  shouldKeepBranch?: (
-    branchContents: PermutableMember[],
-    commonEnvironment: CommonEnvironment
-  ) => boolean;
-  getCommonEnvironment: (
-    branchContents: PermutableMember[]
-  ) => CommonEnvironment;
-  areBranchesEquivalent?: (
-    first: {
-      branchContents: PermutableMember[];
-      commonEnvironment: CommonEnvironment;
-    },
-    second: {
-      branchContents: PermutableMember[];
-      commonEnvironment: CommonEnvironment;
-    }
-  ) => boolean;
-  scoreBranch?: (
-    branchContents: PermutableMember[],
-    commonEnvironment: CommonEnvironment
-  ) => number;
-  branchingLimit?: number;
-}): PermutableMember[][] => {
-  const decisionQueue: Decision[] = [];
-};
+// export const depthFirstSearchDecisionTree = <
+//   PermutableMember extends unknown,
+//   CommonEnvironment extends unknown
+// >({
+//   areBranchesEquivalent,
+//   permutable,
+//   shouldKeepBranch,
+//   getCommonEnvironment,
+//   branchingLimit = Number.MAX_SAFE_INTEGER,
+//   scoreBranch,
+// }: {
+//   permutable: PermutableMember[];
+//   shouldKeepBranch?: (
+//     branchContents: PermutableMember[],
+//     commonEnvironment: CommonEnvironment
+//   ) => boolean;
+//   getCommonEnvironment: (
+//     branchContents: PermutableMember[]
+//   ) => CommonEnvironment;
+//   areBranchesEquivalent?: (
+//     first: {
+//       branchContents: PermutableMember[];
+//       commonEnvironment: CommonEnvironment;
+//     },
+//     second: {
+//       branchContents: PermutableMember[];
+//       commonEnvironment: CommonEnvironment;
+//     }
+//   ) => boolean;
+//   scoreBranch?: (
+//     branchContents: PermutableMember[],
+//     commonEnvironment: CommonEnvironment
+//   ) => number;
+//   branchingLimit?: number;
+// }): PermutableMember[][] => {
+//   const decisionQueue: Decision[] = [];
+// };
+
+export function* depthFirstSearchDecisionTree() {}
